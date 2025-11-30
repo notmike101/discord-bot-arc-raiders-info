c53b07499226bad86144cf09331aa870a2f814e8
Mike Orozco <me@mikeorozco.dev>
2025-11-30 01:26:07 -0600

    [CHORE] Remove unused environment variable

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -4,7 +4,6 @@ declare global {
       readonly DISCORD_TOKEN: string;
       readonly DISCORD_CLIENT_ID: string;
       readonly DISCORD_GUILD_ID: string;
-      readonly GITHUB_API_TOKEN: string;
       readonly REDIS_HOST: string;
       readonly SYNC_INTERVAL: string;
     }
